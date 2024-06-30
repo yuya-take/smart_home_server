e52feebf940532148972b03fb295920bb4310825
yuya-take <yuya970405@gmail.com>
2024-06-30 21:02:58 +0900

    fix logger

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -1,28 +1,28 @@
 import logging
 import os
-
-
 from dotenv import load_dotenv
 
 load_dotenv(verbose=True)
 
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
+# 環境変数からログレベルを取得し、デフォルトをINFOに設定
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
 
 
 def setup_logger():
     logger = logging.getLogger("smart_home_logger")
-    logger.setLevel(LOG_LEVEL)
+    logger.setLevel(getattr(logging, LOG_LEVEL, logging.INFO))
 
-    # コンソールハンドラ
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-
-    # フォーマッター
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    ch.setFormatter(formatter)
-
-    # ハンドラをロガーに追加
+    # ハンドラが既に設定されていないか確認
     if not logger.hasHandlers():
+        # コンソールハンドラ
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+
+        # フォーマッター
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+        ch.setFormatter(formatter)
+
+        # ハンドラをロガーに追加
         logger.addHandler(ch)
 
     logger.info(f"Logger is set up with log level {LOG_LEVEL}")
