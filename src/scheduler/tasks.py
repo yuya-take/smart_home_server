cabdc619473100fbb78bd19e07256a628555f69d
yuya-take <yuya970405@gmail.com>
2024-07-04 20:06:11 +0900

    fix send sensor data graph

--- src/scheduler/tasks.py
+++ src/scheduler/tasks.py
@@ -199,8 +199,8 @@ class SmartHomeMonitor:
         schedule.every(5).seconds.do(self.monitor_message_task)
         schedule.every(30).seconds.do(self.monitor_sensor_to_save_data_task)
         schedule.every().hour.at(":00").do(self.monitor_sensor_to_send_message_task)
-        schedule.every().day.at("00:00").do(self.end_of_day_task)
-        # schedule.every(10).seconds.do(self.end_of_day_task)
+        # schedule.every().day.at("00:00").do(self.end_of_day_task)
+        schedule.every(10).seconds.do(self.end_of_day_task)
 
         while True:
             schedule.run_pending()
--- src/utils/create_graph.py
+++ src/utils/create_graph.py
@@ -1,19 +1,40 @@
 from matplotlib import pyplot as plt
+import matplotlib.dates as mdates
 
 
 def create_3axis_graph(x, y1, y2, y3, x_label, y1_label, y2_label, y3_label, title, save_path):
-    fig, ax1 = plt.subplots()
+    fig = plt.figure()
+    fig.subplots_adjust(bottom=0.2)
+    fig.subplots_adjust(right=0.85)
+    fig.subplots_adjust(left=0.15)
+
+    ax1 = fig.add_subplot(1, 1, 1)
     ax2 = ax1.twinx()
     ax3 = ax1.twinx()
 
-    ax1.plot(x, y1, color="r", label=y1_label)
-    ax2.plot(x, y2, color="g", label=y2_label)
-    ax3.plot(x, y3, color="b", label=y3_label)
+    ax1.set_xlabel(x_label, fontsize=18)
+    ax1.set_ylabel(y1_label, fontsize=18, color="red")
+    ax2.set_ylabel(y2_label, fontsize=18, color="green")
+    ax3.set_ylabel(y3_label, fontsize=18, color="blue")
 
-    ax1.set_xlabel(x_label)
-    ax1.set_ylabel(y1_label, color="r")
-    ax2.set_ylabel(y2_label, color="g")
-    ax3.set_ylabel(y3_label, color="b")
+    rspine = ax3.spines["right"]
+    rspine.set_position(("axes", 1.2))
+
+    ax1.plot(x, y1, color="red", label=y1_label)
+    ax2.plot(x, y2, color="green", label=y2_label)
+    ax3.plot(x, y3, color="blue", label=y3_label)
+
+    h1, l1 = ax1.get_legend_handles_labels()
+    h2, l2 = ax2.get_legend_handles_labels()
+    h3, l3 = ax3.get_legend_handles_labels()
+    ax1.legend(h1 + h2 + h3, l1 + l2 + l3, loc="upper right")
+
     plt.title(title)
 
-    plt.savefig(save_path)
+    # X軸の時間ラベルを一時間ごとに表示
+    ax1.xaxis.set_major_locator(mdates.HourLocator(interval=1))
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter("%H-%M"))
+
+    fig.autofmt_xdate()  # X軸のラベルを自動的にフォーマット
+
+    plt.savefig(save_path, dpi=300)
